e70881a6119ec41198ae51b0e29641129a7bf525
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-17 14:21:32 +0530

    StatesCompleted

--- programs/mint-craft-nft-program/src/state/contentAccount.rs
+++ programs/mint-craft-nft-program/src/state/contentAccount.rs
@@ -3,5 +3,15 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct ContentAccount{
+    creator:Pubkey,
+    #[max_len(500)]
+    prompt:String,
+    #[max_len(50)]
+    content_ipfs:String,
+    #[max_len(50)]
+    metadata_ipfs:String,
+    generation_timeStamp:i64,
+    is_minted:bool,
+    mint_address_ata:Pubkey,
 
 }
